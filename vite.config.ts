c6898d145582d3cab384565a538b2f3f950eb447
ATUL ANAND <atul25anand@gmail.com>
2025-09-03 17:19:12 +0530

    Update vite.config.ts to allow any host

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,11 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["*"], // ✅ allow any host
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
